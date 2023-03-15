8f5c7b3f66ac574fafe6dde22d9dffe48e1ff5a3
Ryan Tuplin <ryan.tuplin@bodis.com>
2023-03-15 21:09:43 +0000

    additional user data points added

---
+++ database/migrations/2023_03_15_205403_update_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('age')->nullable();
+            $table->float('weight', 10, 2)->nullable();
+            $table->string('weight_unit')->default('KG');
+            $table->float('height', 10, 2)->nullable();
+            $table->string('height_unit')->default('CM');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
