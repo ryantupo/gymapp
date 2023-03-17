3ed2ef03d7768fd4176b9d67bbdd3fc80cfc3f9f
Ryan Tuplin <ryan.tuplin@bodis.com>
2023-03-17 23:41:11 +0000

    Addition of logic to create extended details object when user is created

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use App\Models\User_extended_detail;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -26,10 +27,20 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        $user_details = User::firstWhere(['name' => $input['name'], 'email' => $input['email']]);
+        $user_id = $user->id;
+
+        User_extended_detail::create([
+            'user_id' => $user_id,
+        ]);
+
+        return $user;
+
     }
 }
