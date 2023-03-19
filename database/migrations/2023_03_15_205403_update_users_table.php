9611bb5fbbb87d6e2f39942b99790d7ec80b3a76
Ryan Tuplin <ryan.tuplin@bodis.com>
2023-03-19 23:27:33 +0000

    Adding gender to user migration

--- database/migrations/2023_03_15_205403_update_users_table.php
+++ database/migrations/2023_03_15_205403_update_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->integer('age')->nullable();
+            $table->string('gender')->nullable();
             $table->float('weight', 10, 2)->nullable();
             $table->string('weight_unit')->default('KG');
             $table->float('height', 10, 2)->nullable();
