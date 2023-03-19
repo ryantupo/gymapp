e5cc61832fb991307ecdc35f222467fea7f59879
Ryan Tuplin <ryan.tuplin@bodis.com>
2023-03-19 23:27:16 +0000

    passing additional data to view

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Models\User_extended_detail;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+
 
 class UsersController extends Controller
 {
@@ -18,9 +20,13 @@ class UsersController extends Controller
 
             $user_details = User_extended_detail::firstWhere(['user_id' => $user['id']]);
 
+            $createdAt = Carbon::parse($user['created_at']);
+            $daysSinceCreation = $createdAt->diffInDays();
+
             $data = [
                 'user' => $user,
-                'user_details' => $user_details
+                'user_details' => $user_details,
+                'days_using_app' => $daysSinceCreation
             ];
 
             // Pass user data to view
