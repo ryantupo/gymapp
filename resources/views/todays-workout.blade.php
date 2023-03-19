3e5e0fd5b71a9dc34c5276fa4bcb473e6fabdb09
Ryan Tuplin <ryan.tuplin@bodis.com>
2023-03-19 23:45:30 +0000

    Add new menu and blade for todays workout

---
+++ resources/views/todays-workout.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div style="display:flex;flex-direction: column" class="p-10">
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10">
+
+                <h1>this will have todays workout</h1>
+            </div>
+        </div>
+
+    </div>
+</x-app-layout>
