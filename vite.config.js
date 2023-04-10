f54c9363ecd8d02c8431820db0f2f0289193142d
Ryan Tuplin <ryan.tuplin@bodis.com>
2023-04-10 23:12:10 +0100

    starting work on lighttheme/darktheme toggle

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/light.css',
+                'resources/css/dark.css',
                 'resources/js/app.js',
+                'resources/js/theme-switcher.js',
             ],
             refresh: [
                 ...refreshPaths,
